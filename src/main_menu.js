31309fca8e5bab3af6331fc8225878a2d89389cd
LoboArkano <pipiolo_mananes@hotmail.com>
2020-09-22 12:47:57 -0500

    Update start button - If is clicked will start the forest scene

--- src/main_menu.js
+++ src/main_menu.js
@@ -13,8 +13,25 @@ class mainMenu extends Phaser.Scene {
   create() {
     this.image = this.add.image(480, 240, 'mainMenu');
     this.title = this.add.text(400, 100, 'RPG GAME', { fontSize: '42px', fill: '#fff' });
-    this.start = this.add.text(400, 300, 'START', { fontSize: '30px', fill: '#fff' });
-    this.controlls = this.add.text(400, 350, 'CONTROLLS', { fontSize: '30px', fill: '#fff' });
+    this.startBtn = this.add.text(400, 300, 'START', { fontSize: '30px', fill: '#fff' });
+    const controlls = this.add.text(400, 350, 'CONTROLLS', { fontSize: '30px', fill: '#fff' });
+
+    this.startBtn.setInteractive()
+      .on('pointerdown', () => this.startForestScene())
+      .on('pointerover', () => this.startBtnHoverState())
+      .on('pointerout', () => this.startBtnRestState());
+  }
+
+  startForestScene() {
+    this.scene.start('forest');
+  }
+
+  startBtnHoverState() {
+    this.startBtn.setStyle({ fill: '#ff0' });
+  }
+
+  startBtnRestState() {
+    this.startBtn.setStyle({ fill: '#fff' });
   }
 }
 
