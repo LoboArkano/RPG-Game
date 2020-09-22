9dbe8aad4232760d573555bb82233ae7e2d8a91c
LoboArkano <pipiolo_mananes@hotmail.com>
2020-09-22 11:03:09 -0500

    Install file loader

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,14 @@ module.exports = {
     filename: 'build.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
+  },
 };
