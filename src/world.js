31b12228b2d04f1fc98542bbcec2400d1543e647
LoboArkano <pipiolo_mananes@hotmail.com>
2020-09-28 17:36:21 -0500

    Fix bug with name mapJSON

--- src/world.js
+++ src/world.js
@@ -1,7 +1,7 @@
 import Phaser from 'phaser';
 import worldA2 from './assets/images/tileset/World_A2.png';
 import worldB from './assets/images/tileset/World_B.png';
-import mapJSON from './assets/images/maps/worldMap.json';
+import mapWorld from './assets/images/maps/worldMap.json';
 import actor from './assets/images/characters/Protas.png';
 
 let direction = 'standDown';
@@ -14,12 +14,12 @@ class forest extends Phaser.Scene {
   preload() {
     this.load.image('World_A2', worldA2);
     this.load.image('World_B', worldB);
-    this.load.tilemapTiledJSON('mappy', mapJSON);
+    this.load.tilemapTiledJSON('mappyWorld', mapWorld);
     this.load.spritesheet('actor', actor, { frameWidth: 48, frameHeight: 48 });
   }
 
   create() {
-    const mappy = this.add.tilemap('mappy');
+    const mappy = this.add.tilemap('mappyWorld');
     const worldA2Set = mappy.addTilesetImage('World_A2');
     const worldBSet = mappy.addTilesetImage('World_B');
 
@@ -28,10 +28,10 @@ class forest extends Phaser.Scene {
     const locationsLayer = mappy.createStaticLayer('locations', worldBSet, 0, 0).setDepth(-1);
 
     player = this.physics.add.sprite(936, 1174, 'actor');
-    objectsLayer.setCollisionByProperty({ collides: true });
-    // objectsLayer.setCollisionByExclusion([-1]);
-    // objectsLayer.setCollision([338, 373, 389, 417, 418, 419, 420, 433, 434, 435, 436]);
     this.physics.add.collider(player, objectsLayer);
+    // objectsLayer.setCollisionByProperty({ collides: true });
+    objectsLayer.setCollisionByExclusion([-1]);
+    // objectsLayer.setCollision([338, 373, 389, 417, 418, 419, 420, 433, 434, 435, 436]);
 
     this.physics.world.bounds.width = mappy.widthInPixels;
     this.physics.world.bounds.height = mappy.heightInPixels;
