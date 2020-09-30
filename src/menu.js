84074990a2ce03c8a148280ac57f760775acf396
LoboArkano <pipiolo_mananes@hotmail.com>
2020-09-30 17:31:21 -0500

    Add Menus classes

---
+++ src/menu.js
@@ -0,0 +1,116 @@
+import Phaser from 'phaser';
+
+const MenuItem = new Phaser.Class({
+  Extends: Phaser.GameObjects.Text,
+
+  initialize:
+
+  function MenuItem(x, y, text, scene) {
+    Phaser.GameObjects.Text.call(this, scene, x, y, text, { color: '#ffffff', align: 'left', fontSize: 15 });
+  },
+
+  select() {
+    this.setColor('#f8ff38');
+  },
+
+  deselect() {
+    this.setColor('#ffffff');
+  },
+});
+
+const Menu = new Phaser.Class({
+  Extends: Phaser.GameObjects.Container,
+
+  initialize:
+
+  function Menu(x, y, scene, heroes) {
+    Phaser.GameObjects.Container.call(this, scene, x, y);
+    this.menuItems = [];
+    this.menuItemIndex = 0;
+    this.heroes = heroes;
+    this.x = x;
+    this.y = y;
+  },
+
+  addMenuItem(unit) {
+    const menuItem = new MenuItem(0, this.menuItems.length * 20, unit, this.scene);
+    this.menuItems.push(menuItem);
+    this.add(menuItem);
+  },
+
+  moveSelectionUp() {
+    this.menuItems[this.menuItemIndex].deselect();
+    this.menuItemIndex -= 1;
+    if (this.menuItemIndex < 0) this.menuItemIndex = this.menuItems.length - 1;
+    this.menuItems[this.menuItemIndex].select();
+  },
+
+  moveSelectionDown() {
+    this.menuItems[this.menuItemIndex].deselect();
+    this.menuItemIndex += 1;
+    if (this.menuItemIndex >= this.menuItems.length) this.menuItemIndex = 0;
+    this.menuItems[this.menuItemIndex].select();
+  },
+
+  // select the menu as a whole and an element with index from it
+  select(index) {
+    if (!index) index = 0;
+    this.menuItems[this.menuItemIndex].deselect();
+    this.menuItemIndex = index;
+    this.menuItems[this.menuItemIndex].select();
+  },
+
+  // deselect this menu
+  deselect() {
+    this.menuItems[this.menuItemIndex].deselect();
+    this.menuItemIndex = 0;
+  },
+
+  confirm() {
+    // wen the player confirms his slection, do the action
+  },
+});
+
+const HeroesMenu = new Phaser.Class({
+  Extends: Menu,
+
+  initialize:
+
+  function HeroesMenu(x, y, scene) {
+    Menu.call(this, x, y, scene);
+  },
+});
+
+const ActionsMenu = new Phaser.Class({
+  Extends: Menu,
+
+  initialize:
+
+  function ActionsMenu(x, y, scene) {
+    Menu.call(this, x, y, scene);
+    this.addMenuItem('Attack');
+  },
+  confirm() {
+    // do something when the player selects an action
+  },
+
+});
+
+const EnemiesMenu = new Phaser.Class({
+  Extends: Menu,
+
+  initialize:
+
+  function EnemiesMenu(x, y, scene) {
+    Menu.call(this, x, y, scene);
+  },
+  confirm() {
+    // do something when the player selects an enemy
+  },
+});
+
+export {
+  HeroesMenu,
+  ActionsMenu,
+  EnemiesMenu,
+};
--- src/ui.js
+++ src/ui.js
@@ -1,4 +1,7 @@
 import Phaser from 'phaser';
+import {
+  HeroesMenu, ActionsMenu, EnemiesMenu,
+} from './menu';
 
 class ui extends Phaser.Scene {
   constructor() {
@@ -15,6 +18,21 @@ class ui extends Phaser.Scene {
     this.graphics.strokeRect(324, 300, 320, 179);
     this.graphics.fillRect(645, 300, 315, 179);
     this.graphics.strokeRect(645, 300, 315, 179);
+
+    // basic container to hold all menus
+    this.menus = this.add.container();
+
+    this.heroesMenu = new HeroesMenu(661, 320, this);
+    this.actionsMenu = new ActionsMenu(340, 320, this);
+    this.enemiesMenu = new EnemiesMenu(17, 320, this);
+
+    // the currently selected menu
+    this.currentMenu = this.actionsMenu;
+
+    // add menus to the container
+    this.menus.add(this.heroesMenu);
+    this.menus.add(this.actionsMenu);
+    this.menus.add(this.enemiesMenu);
   }
 
   update() {
